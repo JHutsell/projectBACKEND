a219f9a9755fee5971384cb1bfb722a381fe979e
Justin Hutsell <hutselljustin@gmail.com>
2019-09-25 19:55:16 -0400

    heroku frontend

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -10,7 +10,6 @@ class Api::V1::SessionsController < ApplicationController
         scope: "user-read-email user-top-read user-read-currently-playing user-read-playback-state user-read-private playlist-modify-public playlist-modify-private playlist-read-private user-read-recently-played user-library-read",
         show_dialog: true
       }
-      byebug
   
       url = "https://accounts.spotify.com/authorize"
       redirect_to "#{url}?#{query_params.to_query}"
